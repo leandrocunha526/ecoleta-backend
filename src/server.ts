162c5d4c9d74f07ba5687be316b0ffe5a732efc6
leandrocunha526 <leandrocunha016@gmail.com>
2022-01-28 19:00:19 -0300

    Add morgan

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,15 @@ import routes from "./routes";
 import path from "path";
 import cors from "cors";
 import { errors } from "celebrate";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(morgan('dev')); //Morgan logger
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
